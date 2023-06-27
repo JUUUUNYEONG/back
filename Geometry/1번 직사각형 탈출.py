2cca83d9fc864d40c46944b2f4e5722e78bfd73c
JUUUUNYEONG <129607332+JUUUUNYEONG@users.noreply.github.com>
2023-06-27 11:26:00 +0900

    Delete Geometry directory

--- Geometry/1번 직사각형 탈출.py
+++
@@ -1,3 +0,0 @@
-import sys
-x,y,w,h=map(int,sys.stdin.readline().rstrip().split())
-print(min(h-y,y,w-x,x))
--- Geometry/5번 택시 기하학.py
+++
@@ -1,4 +0,0 @@
-from math import pi
-N=int(input())
-print("{:0.6f}".format(pi*N**2))
-print("{:0.6f}".format(2*N**2))
