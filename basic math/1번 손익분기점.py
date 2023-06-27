41cd20b20508c1d0050cc46c10cccd75f8a1d1c6
JUUUUNYEONG <129607332+JUUUUNYEONG@users.noreply.github.com>
2023-06-27 11:25:10 +0900

    Delete basic math directory

--- basic math/1번 손익분기점.py
+++
@@ -1,6 +0,0 @@
-a,b,c=map(int,input().split())
-if c - b > 0:
-    k = int(a/(c-b)) + 1
-    print(k)
-else:
-    print(-1)
--- basic math/2번 벌집.py
+++
@@ -1,7 +0,0 @@
-a=int(input())
-sum=1
-cnt=1
-while a>sum:
-    sum += 6 * cnt
-    cnt += 1
-print(cnt)
