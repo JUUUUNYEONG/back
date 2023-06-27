10f520a58dd349e91a462dd88a24b39bc99c5100
JUUUUNYEONG <129607332+JUUUUNYEONG@users.noreply.github.com>
2023-06-27 11:25:17 +0900

    Delete String directory

--- String/3번 알파벳 찾기.py
+++
@@ -1,3 +0,0 @@
-s=input()
-for i in "abcdefghijklmnopqrstuvwxyz":
-    print(s.find(i),end=" ")
--- String/6번 단어의 개수.py
+++
@@ -1,7 +0,0 @@
-a=list(input().split())
-while " " in a:
-    a.remove(" ")
-print(len(a))
-#split()은 디폴트가 모든 공백을 없애주네요;;
-# a=input().split()
-# print(a)
